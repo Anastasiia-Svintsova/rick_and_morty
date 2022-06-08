80d09a18cdfa4f07d81e269dce3f8333b75f9d75
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-06-08 16:52:15 +0300

    Character card was implemented

--- rick_and_morty_app/src/App.tsx
+++ rick_and_morty_app/src/App.tsx
@@ -1,44 +1,22 @@
-import React, {
-  FC, useCallback, useEffect, useState,
-} from 'react';
-import { getCharacters } from './api/api';
-import './App.css';
-import { CharacterList } from './components/CharacterList/CharacterList';
-import { Loader } from './components/Loader';
-import { ICharacter } from './types/Character';
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.scss';
+import HomePage from './components/HomePage/HomePage';
 
-const App: FC = () => {
-  const [pagesAmount, setPagesAmount] = useState(0);
-  const [charactersAmount, setCharactersAmount] = useState(0);
-  const [characters, setCharacters] = useState<ICharacter[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const loadCharacters = useCallback(async () => {
-    setIsLoading(true);
-    const data = await getCharacters();
-    setIsLoading(false);
-    setCharacters(data.results);
-    setPagesAmount(data.info.pages);
-    setCharactersAmount(data.info.count);
-  }, []);
-
-  useEffect(() => {
-    loadCharacters();
-  }, []);
-
-  return (
-    <div className="App">
-      {!!charactersAmount && (
-        <p>{`${charactersAmount} characters found`}</p>
-      )}
-
-      {isLoading && <Loader />}
-
-      {characters && <CharacterList characters={characters} />}
-
-      {pagesAmount}
+const App: FC = () => (
+  <div className="App">
+    <div className="header">
+      <h1 className="text-center">
+        Rick & Morty
+      </h1>
     </div>
-  );
-};
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/characters/:id" />
+    </Routes>
+  </div>
+);
 
 export default App;
--- rick_and_morty_app/src/components/Character/Character.tsx
+++ rick_and_morty_app/src/components/Character/Character.tsx
@@ -1,17 +1,54 @@
-import React, { FC } from 'react';
+import classNames from 'classnames';
+import React, { FC, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ICharacter } from '../../types/Character';
+import './Character.scss';
 
 interface Props {
   character: ICharacter;
 }
 
 const Character: FC<Props> = ({ character }) => {
-  const { name, image } = character;
+  const {
+    id,
+    name,
+    image,
+    status,
+    species,
+    gender,
+  } = character;
+
+  const navigate = useNavigate();
+
+  const handleClick = useCallback(() => {
+    navigate(`/character/${id}`);
+  }, []);
 
   return (
-    <div className="character">
-      <img src={image} alt={`${name}`} />
-      <p>{name}</p>
+    <div
+      className="character"
+      onClick={handleClick}
+      onKeyDown={handleClick}
+      role="link"
+      tabIndex={0}
+    >
+      <div className="character__photo-container">
+        <img src={image} alt={`${name}`} className="character__photo" />
+      </div>
+
+      <div className="character__additional-info">
+        <p
+          className={classNames(
+            'character__status',
+            { 'character__status--dead': status === 'Dead' },
+            { 'character__status--unknown': status === 'unknown' },
+          )}
+        >
+          {status}
+        </p>
+        <p>{`Name: ${name}`}</p>
+        <p>{`${species} - ${gender}`}</p>
+      </div>
     </div>
   );
 };
